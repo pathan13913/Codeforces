2e0916280a8e32f145f0ebe9e3153ac66c465a57
Arafat Khan Pathan <163514145+pathan13913@users.noreply.github.com>
2024-11-27 08:09:57 +0600

    Add files via upload

---
+++ 231A__Team.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    int count = 0;
+    for (int i = 0; i < t; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a + b + c >= 2)
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
